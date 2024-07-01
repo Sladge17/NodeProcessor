7aa7dcc1be3990063246008d145b1b93bb69eab8
Maxim Sosov <sladge17@gmail.com>
2024-07-01 10:25:50 +0300

    create template for operator

--- __init__.py
+++ __init__.py
@@ -2,13 +2,32 @@ import bpy
 
 
 
+class MESH_OT_node_searcher(bpy.types.Operator):
+    """Search useless nodes"""
+    bl_idname = "material.node_searcher"
+    bl_label = "Node searcher"
+    bl_description = "Search useless nodes"
+    bl_options = {'UNDO'}
+
+
+    @classmethod
+    def poll(cls, context):
+        return True
+
+
+    def execute(self, context):
+        print("test printing")
+        return {'FINISHED'}
+
+
+
 def register():
-    pass
+    bpy.utils.register_class(MESH_OT_node_searcher)
 
 
 
 def unregister():
-    pass
+    bpy.utils.unregister_class(MESH_OT_node_searcher)
 
 
 
