253e4950d6eded5dfc29f7b29fb230ffed3c0a03
Maxim Sosov <sladge17@gmail.com>
2024-07-01 09:31:31 +0300

    first commit

---
+++ __init__.py
@@ -0,0 +1,31 @@
+import bpy
+
+
+
+def register():
+    pass
+
+
+
+def unregister():
+    pass
+
+
+
+bl_info = {
+    "name": "Node Searcher",
+    "author": "Sosov Maxim",
+    "version": (1, 0),
+    "blender": (3, 6, 0),
+    "category": "",
+    "location": "",
+    "description": "",
+    "warning": "",
+    "doc_url": "",
+    "wiki_url": "",
+}
+
+
+
+if __name__ == "__main__":
+    register()
